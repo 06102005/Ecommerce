548660f97303641f8f6bb9e622e460361110e3c7
Divyadharshini <divya.y08092006@gmail.com>
2025-12-25 13:30:41 +0530

    mongo db connection setup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -16,3 +20,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
