5b82e27620163720b16917eeef23450149f02c9a
Chan 2127 <chandru@gmail.com>
2026-01-01 15:51:05 +0530

    Added frontend

--- docs/cart.js
+++ docs/cart.js
@@ -1,12 +1,29 @@
-const cart = JSON.parse(localStorage.getItem("cart")) || [];
-const list = document.getElementById("cartItems");
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let cartItems = document.getElementById("cartItems");
+let total = 0;
 
-if (cart.length === 0) {
-  list.innerHTML = "<li>Cart is empty</li>";
-}
+cartItems.innerHTML = "";
 
-cart.forEach(item => {
-  const li = document.createElement("li");
-  li.textContent = `${item.name} - $${item.price}`;
-  list.appendChild(li);
+
+cart.forEach((item, index) => {
+  total += item.price * item.qty;
+
+  cartItems.innerHTML += `
+    <div class="cart-item">
+      <img src="${item.image}">
+      <div>
+        <h4>${item.name}</h4>
+        <p>$${item.price} × ${item.qty}</p>
+        <button onclick="removeItem(${index})">Remove</button>
+      </div>
+    </div>
+  `;
 });
+
+document.getElementById("cartTotal").innerText = total.toFixed(2);
+
+function removeItem(index) {
+  cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  location.reload();
+}
--- docs/product.js
+++ docs/product.js
@@ -1,16 +1,37 @@
 const params = new URLSearchParams(window.location.search);
 
 const name = params.get("name");
-const price = params.get("price");
+const price = parseFloat(params.get("price"));
 const image = params.get("image");
+const rating = params.get("rating") || "⭐⭐⭐⭐";
+document.getElementById("productRating").innerText = rating;
 
-document.getElementById("name").innerText = name;
-document.getElementById("price").innerText = price;
-document.getElementById("image").src = image;
+const desc = params.get("desc");
+
+document.getElementById("productName").innerText = name;
+document.getElementById("productPrice").innerText = price;
+document.getElementById("productImg").src = image;
+document.getElementById("productDesc").innerText = desc;
+
+let quantity = 1;
+document.getElementById("totalPrice").innerText = price;
+
+function changeQty(val) {
+  quantity += val;
+  if (quantity < 1) quantity = 1;
+
+  document.getElementById("qty").innerText = quantity;
+  document.getElementById("totalPrice").innerText =
+    (price * quantity).toFixed(2);
+}
 
 function addToCart() {
   let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.push({ name, price });
+
+  cart.push({ name, price, image, qty: quantity });
+
   localStorage.setItem("cart", JSON.stringify(cart));
-  alert("Added to cart");
+
+  updateCartCount();
+  alert("Product added to cart 🛒");
 }
